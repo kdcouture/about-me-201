a95c46280e4896b0e8de751559fe68cf053e86cf
Kevin Couture <kcouture939@gmail.com>
2019-06-04 14:56:41 -0700

    Fixed the use of quotes

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ anybody = anybody.toUpperCase(); // Convert to all upper case.
 if(anybody === 'YES' || anybody === 'Y'){
   alert('Good, good. I was worried I was talking to myself.')
 }
-else if(anybody === "NO" || anybody === 'N') {
+else if(anybody === 'NO' || anybody === 'N') {
   alert('Well I guess I\'m still going to ask the next questions...');
 }
 else {
@@ -31,7 +31,7 @@ bus = bus.toUpperCase(); // Convert to all upper case.
 if(bus === 'YES' || bus === 'Y'){
   alert('Nice, I also ride the bus from Maple Valley. It only takes me a hour and a half to get down here. It\'s great!... /s');
 }
-else if(bus === "NO" || bus === 'N') {
+else if(bus === 'NO' || bus === 'N') {
   alert('Yea it kinda of is not so fun');
 }
 else {
@@ -41,13 +41,13 @@ else {
 var car = prompt('Do you have a car?');
 console.log('car?: ' + car);
 car = car.toUpperCase(); // Convert to all upper case.
-if( (car === 'YES' || car === "Y") && (bus === 'Y' && bus === "YES")) {
+if( (car === 'YES' || car === 'Y') && (bus === 'Y' && bus === 'YES')) {
   alert('Yea I wouldn\'t want to drive to Seattle either... In fact I don\'t want to do that.');
 }
 if(car === 'YES' || car === 'Y'){
   alert('Nice, I bet it gets you from A to B huh?');
 }
-else if(car === "NO" || car === 'N') {
+else if(car === 'NO' || car === 'N') {
   alert('Well at least the bus is an option. Walking is never too bad either.');
 }
 else {
@@ -60,7 +60,7 @@ cat = cat.toUpperCase(); // Convert to all upper case.
 if(cat === 'YES' || cat === 'Y'){
   alert('Sweet! I have 4 cats right now.');
 }
-else if(cat === "NO" || cat === 'N') {
+else if(cat === 'NO' || cat === 'N') {
   alert('Wack, you should have a cat.');
 }
 else {
@@ -73,7 +73,7 @@ stoked = stoked.toUpperCase(); // Convert to all upper case.
 if(stoked === 'YES' || stoked === 'Y'){
   alert('Cool, I know I am. A whole lot of cool stuff starting for me.');
 }
-else if(stoked === "NO" || stoked === 'N') {
+else if(stoked === 'NO' || stoked === 'N') {
   alert('Dang it is almost half over, better do something to spice it up.');
 }
 else {
