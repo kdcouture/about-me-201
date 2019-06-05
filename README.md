4f46467c00f9e860ea7381791f6aa73c5d9d744e
Kevin Couture <kcouture939@gmail.com>
2019-06-05 14:49:04 -0700

    Updated to include Lab 3 requirements.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # about-me-201
 Kevin Couture Lab2 about me
+Created Jun. 4th, 2019
+
+Jun. 5th, 2019
+---------------
+Extended into lab 3 by expanding on lab 2 material. 
+Included addition questions while converting original question to be more focused "about me". 
+Added lists to the project.
--- js/app.js
+++ js/app.js
@@ -1,81 +1,120 @@
 /*
 Kevin Couture
 Lab2
-Jun. 4th, 2019
+Created: Jun. 4th, 2019
 app.js
 
 The goal of lab 2 is to create 5 yes/no questions with a response
 for yes and for no. Each response is logged into the console.
 */
 
+// Because you told me to.
 'use strict';
 
-// Below are 5 questions with response for yes and for no.
+// Get the users name.
+var uname = prompt('Please enter your name.');
 
-var anybody = prompt('Is anybody out there?');
-console.log('anyone?: ' + anybody);
-anybody = anybody.toUpperCase(); // Convert to all upper case.
-if(anybody === 'YES' || anybody === 'Y'){
-  alert('Good, good. I was worried I was talking to myself.')
+// A variable to store the number of correct guesses.
+var correctAns = 0;
+
+// A variable to hold the number of guesses used.
+var guesses = 0;
+
+// Create a random number for number guessing question.
+var rNum = Math.floor(Math.random()*10);
+console.log('The random number is... ' + rNum);
+
+// Correct responses stored in array, two arrays are used to account for two possible answers.
+var correctArray = ['Y','Y','Y','Y','Y'];
+var correctArray2 = ['YES','YES','YES','YES','YES'];
+console.log('Correct answers are... ' + correctArray);
+console.log('Other accepted answers are... ' + correctArray2);
+
+// This array stores the correct responses for multiple answer question.
+var mAns = ['TEMPY', 'MEOWZERS', 'ANGLE CLOUD', 'MEOWDELHIME'];
+console.log('The multiple possible answers are... ' + mAns);
+
+//
+// Below are two parralel arrays that are used to decrease amount of code required.
+//
+// Questions stored in array for condesing the code.
+var qArray = ['Is anybody out there?', 'Do I take the bus for my commute?', 'Do I have a car?', 'Do I have at least 1 cat?', 'Am I stoked for this year?'];
+// Alert Responses stored here. 2(n) is the response for yes, 2(n)+1 is the response for no.
+var rArray = ['Good, good. I was worried I was talking to myself.', 'Well I guess I\'m still going to ask the next questions...', 'It\'s great!... /s', 'Well I do and yea it kinda of is not so fun', 'Nice, it gets me from A to B.', 'Well I can see why you think that on acount of the bus and all.', 'Sweet!', 'Wack', 'Cool, a whole lot of cool stuff starting for me.', 'Geez I know I\'m monotone but I am stoked about it since work started.']
+
+//
+// Below is a loop that will ask 5 questions with response for yes and for no.
+//
+for(var i = 0; i < 5; i++){
+  var resp = prompt(qArray[i]);
+  console.log(qArray[i] + ' ' + resp);
+  resp = resp.toUpperCase(); // Convert to all upper case.
+  if(resp === correctArray[i] || resp === correctArray2[i]){
+    alert(rArray[2*i]);
+    correctAns++;
+  }
+  else {
+    alert(rArray[2*i+1]);
+  }
 }
-else if(anybody === 'NO' || anybody === 'N') {
-  alert('Well I guess I\'m still going to ask the next questions...');
-}
-else {
-  alert('Invalid input!');
+//
+// Ask a question with 4 chances.
+//
+guesses = 4;
+// Variable declartion for storge of users guess.
+var numGuess;
+while(guesses > 0) {
+  numGuess = prompt('Guess what number I am thinking between 1 and 10.');
+  console.log('Number guessed by user: ' + numGuess + ' The random number is: ' + rNum);
+  if(rNum < numGuess) {
+    alert('Try a smaller number.');
+  }
+  else if(rNum > numGuess) {
+    alert('Try a larger number.');
+  }
+  else if(rNum == numGuess) {
+    alert('Wow you got it!');
+    correctAns++;
+    console.log('They guessed the number correctly.');
+    break;
+  }
+  guesses--;
 }
 
-var bus = prompt('Do you take the bus for your commute');
-console.log('bus ride?: ' + bus);
-bus = bus.toUpperCase(); // Convert to all upper case.
-if(bus === 'YES' || bus === 'Y'){
-  alert('Nice, I also ride the bus from Maple Valley. It only takes me a hour and a half to get down here. It\'s great!... /s');
-}
-else if(bus === 'NO' || bus === 'N') {
-  alert('Yea it kinda of is not so fun');
-}
-else {
-  alert('Invalid input!');
-}
+//
+// Ask a question with 6 chances to get one correct answer.
+//
+guesses = 5;
+// Storage for users guess.
+var catName;
+// Correct answer flag.
+var isNameFlag = false;
+do{
+  catName = prompt('Can you guess one of my (four) cat\'s name?').toUpperCase(); // Is stored in upper casing.
+  // Compair against each name.
+  for(var idx = 0; idx < mAns.length; idx++) {
+    console.log('Name Guessed is: ' + catName);
+    // If correct, break out of loop using flag.
+    if(catName === mAns[idx]) {
+      alert('Nice, you got one!');
+      correctAns++;
+      isNameFlag = true; // Raise flag
+      break;
+    }
+  }
+  // Name not correct.
+  if(!isNameFlag){
+    alert('Nope! You have ' + guesses + ' remaining.');
+  }
+  // Name was correct, break out of loop.
+  else {
+    console.log('They got one right!');
+    alert('The correct options were... ' + mAns);
+    break;
+  }
+  // Decrement guess count.
+  guesses--;
+} while (guesses >= 0);
 
-var car = prompt('Do you have a car?');
-console.log('car?: ' + car);
-car = car.toUpperCase(); // Convert to all upper case.
-if( (car === 'YES' || car === 'Y') && (bus === 'Y' && bus === 'YES')) {
-  alert('Yea I wouldn\'t want to drive to Seattle either... In fact I don\'t want to do that.');
-}
-if(car === 'YES' || car === 'Y'){
-  alert('Nice, I bet it gets you from A to B huh?');
-}
-else if(car === 'NO' || car === 'N') {
-  alert('Well at least the bus is an option. Walking is never too bad either.');
-}
-else {
-  alert('Invalid input!');
-}
-
-var cat = prompt('Do you have at least 1 cat?');
-console.log('cat?: ' + cat);
-cat = cat.toUpperCase(); // Convert to all upper case.
-if(cat === 'YES' || cat === 'Y'){
-  alert('Sweet! I have 4 cats right now.');
-}
-else if(cat === 'NO' || cat === 'N') {
-  alert('Wack, you should have a cat.');
-}
-else {
-  alert('Invalid input!');
-}
-
-var stoked = prompt('Are you stoked for this year?');
-console.log('stoked?: ' + stoked);
-stoked = stoked.toUpperCase(); // Convert to all upper case.
-if(stoked === 'YES' || stoked === 'Y'){
-  alert('Cool, I know I am. A whole lot of cool stuff starting for me.');
-}
-else if(stoked === 'NO' || stoked === 'N') {
-  alert('Dang it is almost half over, better do something to spice it up.');
-}
-else {
-  alert('Invalid input!');
-}
+console.log(correctAns + ' correct answers.');
+alert('Wow! ' + uname +' you got ' + correctAns + ' correct answers! out of 7 possible');
